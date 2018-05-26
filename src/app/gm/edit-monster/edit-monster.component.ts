eb7b9c7978e686b510dba95c530c5013ae935744
eric grififith <eric@griffithnet.com>
2018-05-26 11:01:00 +0300

    comments

--- src/app/gm/edit-monster/edit-monster.component.ts
+++ src/app/gm/edit-monster/edit-monster.component.ts
@@ -3,51 +3,99 @@ import * as _ from 'lodash';
 
 import { Monster, MonsterInstance } from '../../monster.class';
 import { GmDataService } from '../gm-data.service';
-
+/**
+ * Component for making changes to monster details.
+ *
+ * @export
+ * @class EditMonsterComponent
+ * @implements {OnInit}
+ * @implements {OnChanges}
+ */
 @Component({
   selector: 'edit-monster',
   templateUrl: './edit-monster.component.html',
   styleUrls: ['./edit-monster.component.css']
 })
-export class EditMonsterComponent implements OnInit {
+export class EditMonsterComponent implements OnInit, OnChanges {
 
-	@Input() monster: Monster;
-	@Output() monsterChanges: EventEmitter<Monster> = new EventEmitter();
+	/**
+   * Send the changed monster back when it is saved
+   *
+   * @type {EventEmitter<Monster>}
+   * @memberof EditMonsterComponent
+   */
+  @Output() monsterChanges: EventEmitter<Monster> = new EventEmitter();
+  @Input() monster: Monster;
 
 	@Input() isInstance: boolean = false;
 
 	_monster: Monster;
-
+  /**
+   * Creates an instance of EditMonsterComponent.
+   * @param {GmDataService} gmds
+   * @memberof EditMonsterComponent
+   * @constructor
+   */
   constructor(private gmds: GmDataService) { }
-
-  ngOnInit() {
-  }
-
-	ngOnChanges(changes: SimpleChanges) {
-		for (let prop in changes) {
-			if (prop=='monster') {
-				this._monster = _.cloneDeep(changes[prop].currentValue);
-			} else if (prop=='isInstance') {
-			}
-		}
-	}
 
   /**
    *
+   * @param {SimpleChanges} changes
+   * @returns
+   * @memberof EditMonsterComponent
    */
-	isChanged(): boolean {
+  ngOnChanges(changes: SimpleChanges) {
+    for (let prop in changes) {
+      if (prop=='monster') {
+        this._monster = _.cloneDeep(changes[prop].currentValue);
+      } else if (prop=='isInstance') {
+      }
+    }
+  }
+  /**
+   *
+   *
+   * @memberof EditMonsterComponent
+   */
+  ngOnInit() {}
+  /**
+   * True if the monster has been changed and not saved
+   *
+   * @returns {boolean}
+   * @memberof EditMonsterComponent
+   */
+  isChanged(): boolean {
 		return !_.isEqual(this.monster,this._monster);
 	}
-	save(monster: Monster) {
+  /**
+   * Save the monster when required
+   *
+   * @param {Monster} monster
+   * @memberof EditMonsterComponent
+   */
+  save(monster: Monster) {
 		this.gmds.updateMonster(monster.key, monster);
 		this._monster = _.cloneDeep(this.monster);
-	}
-	reset() {
+  }
+ /**
+  * Restore values over unsaved changes
+  *
+  * @memberof EditMonsterComponent
+  */
+ reset() {
 		this.monster = _.cloneDeep(this._monster);
-	}
+  }
+
 	cancel() {
-	}
-	stringify(obj) {
+  }
+ /**
+  * give view access to stringify function
+  *
+  * @param {any} obj
+  * @returns
+  * @memberof EditMonsterComponent
+  */
+ stringify(obj) {
 		return JSON.stringify(obj);
 	}
 
